38470b60eee04a9901f96c2c9198d9b3ced4dcc2
Zexi. Li <lzx12138@sjtu.edu.cn>
2025-03-06 21:37:49 +0800

    feat: added display and key controls.

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,19 @@ mod note;
 mod parser;
 mod utils;
 
+use std::cell::RefCell;
+use std::cmp::{max, min};
 use std::fs::File;
 use std::io::{self, BufRead};
+use std::rc::Rc;
 
-use crossterm::event::{self, Event};
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::layout::{Constraint, Direction, Layout};
+use ratatui::style::palette::tailwind::SLATE;
+use ratatui::style::{Modifier, Style, Stylize};
+use ratatui::symbols::border;
+use ratatui::text::{Line, Text};
+use ratatui::widgets::{Block, Borders, List, ListItem, Paragraph, Widget};
 use ratatui::{DefaultTerminal, Frame};
 
 fn main() -> io::Result<()> {
@@ -26,6 +35,7 @@ pub struct App {
     input: String,
     input_index: usize,
     current_selection: Option<i32>,
+    current_note_index: usize,
     edit_focus: usize,
     note: Note,
     exit: bool,
@@ -43,6 +53,13 @@ enum EditMode {
     NoteInput,
 }
 
+fn find_note_index(notes: &Vec<RefNote>, note: &RefNote) -> usize {
+    notes
+        .iter()
+        .position(|x| Rc::ptr_eq(x, note))
+        .unwrap_or_default()
+}
+
 impl Default for App {
     fn default() -> App {
         // Read from file
@@ -57,6 +74,7 @@ impl Default for App {
             // note: Note::from(String::from(""), String::from(""), 0),
             note: Note::new(),
             current_selection: Some(0),
+            current_note_index: 1,
             exit: false,
         }
     }
@@ -65,93 +83,8 @@ impl Default for App {
 impl App {
     fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
         while !self.exit {
-            terminal.draw(|frame| self.ui(frame))?;
+            terminal.draw(|frame| self.draw(frame))?;
             self.handle_events()?;
-
-            // if let Event::Key(key) = event::read()? {
-            //     match app.mode {
-            //         AppMode::View => match key.code {
-            //             KeyCode::Char('q') => {
-            //                 // write_to_file(&app.notes);
-            //                 // write_to_file_json(&app.notes);
-            //                 return Ok(());
-            //             }
-            //             KeyCode::Char('e') => {
-            //                 use std::process::Command;
-            //
-            //                 Command::new("nvim")
-            //                     .arg("/tmp/note_rs.tmp")
-            //                     .status()
-            //                     .expect("failed to execute process");
-            //
-            //                 if std::path::Path::new("/tmp/note_rs.tmp").exists() {
-            //                     // Open the file in read-only mode.
-            //                     let file = File::open("/tmp/note_rs.tmp").unwrap();
-            //                     // Read the file line by line, and return an iterator of the lines of the file.
-            //                     let lines = io::BufReader::new(file).lines();
-            //                     for line in lines {
-            //                         if let Ok(note) = line {
-            //                             // app.notes.push(Note {
-            //                             //     tag: String::from(""),
-            //                             //     command: vec![note],
-            //                             // });
-            //                             // app.notes.push(Note::new());
-            //                             break;
-            //                         }
-            //                     }
-            //
-            //                     use std::fs;
-            //                     fs::remove_file("/tmp/note_rs.tmp")?;
-            //                 }
-            //
-            //                 // let mut stdout = io::stdout();
-            //                 // execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
-            //
-            //                 terminal.clear()?;
-            //             }
-            //             KeyCode::Char('j') => {
-            //                 app.current_selection = match app.current_selection {
-            //                     None => None,
-            //                     Some(i) => Some(std::cmp::min(app.notes.len() as i32 - 1, i + 1)),
-            //                 };
-            //             }
-            //             KeyCode::Char('k') => {
-            //                 app.current_selection = match app.current_selection {
-            //                     None => None,
-            //                     Some(i) => Some(std::cmp::max(0, i - 1)),
-            //                 };
-            //             }
-            //             KeyCode::Char('d') => {
-            //                 // Double check to delete
-            //                 if let Event::Key(key2) = event::read()? {
-            //                     match key2.code {
-            //                         KeyCode::Char('d') => match app.current_selection {
-            //                             None => {}
-            //                             Some(index) => {
-            //                                 app.notes.remove(index as usize);
-            //                                 app.current_selection = Some(std::cmp::min(
-            //                                     index,
-            //                                     app.notes.len() as i32 - 1,
-            //                                 ));
-            //                             }
-            //                         },
-            //                         KeyCode::Esc => {}
-            //                         _ => {}
-            //                     }
-            //                 }
-            //             }
-            //
-            //             KeyCode::Char('a') => {
-            //                 app.mode = AppMode::Editing;
-            //                 app.edit_mode = EditMode::Direct;
-            //             }
-            //             _ => {}
-            //         },
-            //         AppMode::Editing => {
-            //             // InputPopup::key_event(&mut app, &key);
-            //         }
-            //     }
-            // }
         }
         Ok(())
     }
@@ -172,61 +105,234 @@ fn read_from_markdown() -> Vec<RefNote> {
 
 impl App {
     fn handle_events(&mut self) -> io::Result<()> {
-        todo!()
+        match event::read()? {
+            // it's important to check that the event is a key press event as
+            // crossterm also emits key release and repeat events on Windows.
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
+            }
+            _ => {}
+        };
+        Ok(())
     }
-    fn ui(&self, frame: &mut Frame) {
-        // let chunks = Layout::default()
-        //     .direction(Direction::Vertical)
-        //     .margin(2)
-        //     // .constraints([Constraint::Length(3), Constraint::Min(1)].as_ref())
-        //     .constraints([Constraint::Min(1)].as_ref())
-        //     .split(frame.size());
-        //
-        // let mut texts: Vec<Spans> = Vec::new();
-        //
-        // let mut index: i32 = 0;
-        // for note in &app.notes {
-        //     if index
-        //         == match app.current_selection {
-        //             None => -1,
-        //             Some(value) => value,
-        //         }
-        //     {
-        //         let hl_style = Style::default().add_modifier(Modifier::BOLD);
-        //         texts.push(note.borrow().format(index, hl_style));
-        //     } else {
-        //         texts.push(note.borrow().format(index, Style::default()));
-        //     }
-        //     index += 1;
-        // }
-        //
-        // // let input_area = Paragraph::new(app.input.as_ref())
-        // //     .style(match app.mode {
-        // //         AppMode::View => Style::default(),
-        // //         AppMode::Editing => Style::default().fg(Color::Yellow),
-        // //     })
-        // //     .block(Block::default().borders(Borders::ALL).title("Input"));
-        // // f.render_widget(input_area, chunks[0]);
-        //
-        // let help_message =
-        //     Paragraph::new(texts).block(Block::default().borders(Borders::ALL).title("Notes"));
-        // frame.render_widget(help_message, chunks[0]);
-        //
-        // match app.mode {
-        //     AppMode::View =>
-        //         // Hide the cursor. `Frame` does this by default, so we don't need to do anything here
-        //         {}
-        //
-        //     AppMode::Editing => {
-        //         // let block = Block::default().title("Input").borders(Borders::ALL);
-        //         let area = centered_rect(60, 40, frame.size());
-        //         // InputPopup::render(f, &area, app);
-        //     }
-        // }
-        //
-        // match app.mode {
-        //     AppMode::Editing => {}
-        //     _ => {}
-        // }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match self.mode {
+            AppMode::View => match key_event.code {
+                KeyCode::Char('q') => {
+                    // write_to_file(&app.notes);
+                    // write_to_file_json(&app.notes);
+                    self.exit = true;
+                }
+                KeyCode::Char('e') => {
+                    use std::process::Command;
+
+                    Command::new("nvim")
+                        .arg("/tmp/note_rs.tmp")
+                        .status()
+                        .expect("failed to execute process");
+
+                    if std::path::Path::new("/tmp/note_rs.tmp").exists() {
+                        // Open the file in read-only mode.
+                        let file = File::open("/tmp/note_rs.tmp").unwrap();
+                        // Read the file line by line, and return an iterator of the lines of the file.
+                        let lines = io::BufReader::new(file).lines();
+                        for line in lines {
+                            if let Ok(note) = line {
+                                // app.notes.push(Note {
+                                //     tag: String::from(""),
+                                //     command: vec![note],
+                                // });
+                                // app.notes.push(Note::new());
+                                break;
+                            }
+                        }
+
+                        use std::fs;
+                        fs::remove_file("/tmp/note_rs.tmp").unwrap();
+                    }
+
+                    // let mut stdout = io::stdout();
+                    // execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+
+                    // terminal.clear();
+                }
+                KeyCode::Char('h') => {
+                    let parent_index = find_note_index(
+                        &self.notes,
+                        self.notes[self.current_note_index]
+                            .borrow()
+                            .parent
+                            .as_ref()
+                            .unwrap(),
+                    );
+                    self.current_note_index = if parent_index == 0 {
+                        self.current_note_index
+                    } else {
+                        parent_index
+                    };
+                }
+                KeyCode::Char('l') => {
+                    let child_index = find_note_index(
+                        &self.notes,
+                        if let Some(note) = self.notes[self.current_note_index]
+                            .borrow()
+                            .children
+                            .first()
+                        {
+                            note.as_ref().unwrap()
+                        } else {
+                            &self.notes[self.current_note_index]
+                        },
+                    );
+                    self.current_note_index = child_index;
+                }
+                KeyCode::Char('j') => {
+                    let parent_index = find_note_index(
+                        &self.notes,
+                        self.notes[self.current_note_index]
+                            .borrow()
+                            .parent
+                            .as_ref()
+                            .unwrap(),
+                    );
+                    let next_sibling_index = find_note_index(
+                        &self.notes,
+                        self.notes
+                            .iter()
+                            .enumerate()
+                            .filter(|(index, x)| {
+                                index > &self.current_note_index
+                                    && Rc::ptr_eq(
+                                        x.borrow().parent.as_ref().unwrap(),
+                                        &self.notes[parent_index],
+                                    )
+                            })
+                            .next()
+                            .unwrap_or((0, &self.notes[self.current_note_index]))
+                            .1,
+                    );
+                    let max_sibling_index = find_note_index(
+                        &self.notes,
+                        self.notes[parent_index]
+                            .borrow()
+                            .children
+                            .last()
+                            .unwrap()
+                            .as_ref()
+                            .unwrap(),
+                    );
+                    self.current_note_index = min(next_sibling_index, max_sibling_index);
+                }
+                KeyCode::Char('k') => {
+                    let parent_index = find_note_index(
+                        &self.notes,
+                        self.notes[self.current_note_index]
+                            .borrow()
+                            .parent
+                            .as_ref()
+                            .unwrap(),
+                    );
+                    let null_ref = Rc::new(RefCell::new(Note::new()));
+                    let prev_sibling_index = find_note_index(
+                        &self.notes,
+                        self.notes
+                            .iter()
+                            .enumerate()
+                            .filter(|(index, x)| {
+                                index < &self.current_note_index
+                                    && Rc::ptr_eq(
+                                        x.borrow().parent.as_ref().unwrap_or_else(|| &null_ref),
+                                        &self.notes[parent_index],
+                                    )
+                            })
+                            .last()
+                            .unwrap_or((0, &self.notes[self.current_note_index]))
+                            .1,
+                    );
+                    let min_sibling_index = find_note_index(
+                        &self.notes,
+                        self.notes[parent_index]
+                            .borrow()
+                            .children
+                            .first()
+                            .unwrap()
+                            .as_ref()
+                            .unwrap(),
+                    );
+                    self.current_note_index = max(prev_sibling_index, min_sibling_index);
+                }
+                KeyCode::Char('d') => {
+                    // Double check to delete
+                    if let Event::Key(key2) = event::read().unwrap() {
+                        match key2.code {
+                            KeyCode::Char('d') => match self.current_selection {
+                                None => {}
+                                Some(index) => {
+                                    self.notes.remove(index as usize);
+                                    self.current_selection =
+                                        Some(std::cmp::min(index, self.notes.len() as i32 - 1));
+                                }
+                            },
+                            KeyCode::Esc => {}
+                            _ => {}
+                        }
+                    }
+                }
+
+                KeyCode::Char('a') => {
+                    self.mode = AppMode::Editing;
+                    self.edit_mode = EditMode::Direct;
+                }
+                _ => {}
+            },
+            AppMode::Editing => {
+                // InputPopup::key_event(&mut app, &key);
+            }
+        }
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        let layouts = Layout::default()
+            .direction(Direction::Horizontal)
+            .margin(2)
+            .constraints(
+                [
+                    Constraint::Fill(1),
+                    Constraint::Fill(1),
+                    Constraint::Fill(1),
+                ]
+                .as_ref(),
+            )
+            .split(frame.area());
+
+        const SELECTED_STYLE: Style = Style::new().bg(SLATE.c800).add_modifier(Modifier::BOLD);
+
+        for depth in 2..4 {
+            // Create a list for the notes with depth
+            let notes_at_depth: Vec<ListItem> = self
+                .notes
+                .iter()
+                .filter(|n| n.borrow().depth == depth)
+                .map(|note| {
+                    if Rc::ptr_eq(note, &self.notes[self.current_note_index]) {
+                        ListItem::from(note.borrow().title.clone()).style(SELECTED_STYLE)
+                    } else {
+                        ListItem::from(note.borrow().title.clone())
+                    }
+                })
+                .collect();
+
+            let list = List::new(notes_at_depth).block(
+                Block::new()
+                    .title(format!("Depth: {}", depth))
+                    .borders(Borders::ALL),
+            );
+            frame.render_widget(list, layouts[depth - 2]);
+        }
+        // Render the note
+        let para = Paragraph::new(self.notes[self.current_note_index].borrow().content.clone())
+            .block(Block::default().borders(Borders::ALL).title("Note"));
+        frame.render_widget(para, layouts[2]);
     }
 }
